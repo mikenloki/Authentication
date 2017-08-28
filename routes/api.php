7c7067859c2a5c315cc3f21a9f53697fa0ff9df5
mikenloki <mikenloki@aol.com>
2017-08-28 15:20:15 -0400

    Finished Authenticator

--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::post('signUp', 'AuthController@signUp');
+Route::post('signIn', 'AuthController@signIn');
+Route::get('getUser', 'AuthController@getUser');
+
 Route::any('{path?}', 'MainController@index')->where("path", ".+");
